2f9d1415d36cea2dfe72829820c33b9d0da63831
pasaunders <kaorti@gmail.com>
2017-10-23 15:23:21 -0700

    sketched out site

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("projects")]
+        public IActionResult Projects()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        [Route("contact")]
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+    }
+}
